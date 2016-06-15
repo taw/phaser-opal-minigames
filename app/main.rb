656f9da9814c6df42ef95e4538e321a17141daea
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2016-06-16 00:46:04 +0100

    Some simple demo

--- app/main.rb
+++ app/main.rb
@@ -1,6 +1,16 @@
 require 'opal'
 require 'opal-phaser'
 
+# This needs to be PRed upstream
+module Phaser
+  class Time
+    alias_native :elapsed
+    alias_native :elapsedMS
+    alias_native :physicsElapsed
+    alias_native :physicsElapsedMS
+  end
+end
+
 class Game
   def initialize
     run
@@ -20,8 +30,20 @@ class MainLevel < Phaser::State
 
   def create
     @star = $game.add.sprite(100, 100, 'star')
+    a = rand* 2 * `Math.PI`
+    speed = 200.0
+    @dx, @dy = speed * `Math.sin(a)`, speed * `Math.cos(a)`
   end
 
   def update
+    dt = $game.time.physicsElapsed
+    @star.x += @dx * dt
+    @star.y += @dy * dt
+    if @star.x >= 200 or @star.x <= 0
+      @dx = -@dx
+    end
+    if @star.y >= 200 or @star.y <= 0
+      @dy = -@dy
+    end
   end
 end
