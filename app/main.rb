a047e0b31422ab455fdeb342481a1fc6e43f0cd0
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2016-06-16 15:17:42 +0100

    Extract monkeypatches to common

--- app/main.rb
+++ app/main.rb
@@ -1,16 +1,4 @@
-require "opal"
-require "opal-phaser"
-require "math"
-
-# This needs to be PRed upstream
-module Phaser
-  class Time
-    alias_native :elapsed
-    alias_native :elapsedMS
-    alias_native :physicsElapsed
-    alias_native :physicsElapsedMS
-  end
-end
+require_relative "common"
 
 class Game
   def initialize
