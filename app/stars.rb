87150427aade9ef829811597059386e804666b93
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2016-06-16 15:48:23 +0100

    10 bouncy stars

--- app/stars.rb
+++ app/stars.rb
@@ -14,20 +14,15 @@ class Game
   end
 end
 
-class MainLevel < Phaser::State
-  def preload
-    $game.load.image("star", "/assets/images/star.png")
-  end
-
-  def create
-    @star = $game.add.sprite(rand*$size_x, rand*$size_y, 'star')
+class Star
+  def initialize
+    @star = $game.add.sprite(rand*($size_x-24), rand*($size_y-22), 'star')
     a = rand * 2 * Math::PI
     speed = 500.0
     @dx, @dy = speed * Math.sin(a), speed * Math.cos(a)
   end
 
-  def update
-    dt = $game.time.physicsElapsed
+  def update(dt)
     @star.x += @dx * dt
     @star.y += @dy * dt
     if @star.x >= $size_x-24 or @star.x <= 0
@@ -35,6 +30,25 @@ class MainLevel < Phaser::State
     end
     if @star.y >= $size_y-22 or @star.y <= 0
       @dy = -@dy
+    end
+  end
+end
+
+class MainLevel < Phaser::State
+  def preload
+    $game.load.image("star", "/assets/images/star.png")
+  end
+
+  def create
+    @elements = 10.times.map do
+      Star.new
+    end
+  end
+
+  def update
+    dt = $game.time.physicsElapsed
+    @elements.each do |element|
+      element.update(dt)
     end
   end
 end
