7b50abef7f876d5f11ea0df6c021f3246227438d
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2016-06-16 22:22:49 +0100

    Range#step

--- app/common.rb
+++ app/common.rb
@@ -3,6 +3,70 @@ require "opal-phaser"
 require "browser"
 
 # This needs to be PRed upstream
+# Copied from rubinius
+class Range
+  def step(step_size=1) # :yields: object
+    return to_enum(:step, step_size) unless block_given?
+    first = @begin
+    last = @end
+
+    if step_size.kind_of? Float or first.kind_of? Float or last.kind_of? Float
+      # if any are floats they all must be
+      begin
+        step_size = Float(from = step_size)
+        first     = Float(from = first)
+        last      = Float(from = last)
+      rescue ArgumentError
+        raise TypeError, "no implicit conversion to float from #{from.class}"
+      end
+    else
+      step_size = Integer(from = step_size)
+      if ! step_size.kind_of? Integer
+        raise TypeError, "can't convert #{from.class} to Integer (#{from.class}#to_int gives #{step_size.class})"
+      end
+    end
+
+    if step_size <= 0
+      raise ArgumentError, "step can't be negative" if step_size < 0
+      raise ArgumentError, "step can't be 0"
+    end
+
+    if first.kind_of?(Float)
+      err = (first.abs + last.abs + (last - first).abs) / step_size.abs * Float::EPSILON
+      err = 0.5 if err > 0.5
+      if @excl
+        n = ((last - first) / step_size - err).floor
+        n += 1 if n * step_size + first < last
+      else
+        n = ((last - first) / step_size + err).floor + 1
+      end
+
+      i = 0
+      while i < n
+        d = i * step_size + first
+        d = last if last < d
+        yield d
+        i += 1
+      end
+    elsif first.kind_of?(Numeric)
+      d = first
+      while @excl ? d < last : d <= last
+        yield d
+        d += step_size
+      end
+    else
+      counter = 0
+      each do |o|
+        yield o if counter % step_size == 0
+        counter += 1
+      end
+    end
+
+    return self
+  end
+end
+
+# This needs to be PRed upstream
 module Phaser
   class Time
     alias_native :elapsed
