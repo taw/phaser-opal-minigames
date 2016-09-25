f87f92ef7eb73b6e7473dcc8f83450cb2202bd04
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2016-09-25 17:51:56 +0100

    Banana trail

--- app/cookie.rb
+++ app/cookie.rb
@@ -12,15 +12,16 @@ end
 class MainState < Phaser::State
   def preload
     $game.load.image('cookie', '/images/cookie.png')
+    $game.load.image('banana', '/images/banana.png')
     $game.load.audio("coin", "/audio/balsa.mp3")
   end
 
   def update
     dt = $game.time.physics_elapsed
-    if @cookie.x < 0 or @cookie.x >= $size_x-120
+    if @cookie.x < 60 or @cookie.x >= $size_x-60
       @dx = -@dx
     end
-    if @cookie.y < 0 or @cookie.y >= $size_y-120
+    if @cookie.y < 60 or @cookie.y >= $size_y-60
       @dy = -@dy
     end
     @cookie.x += @dx*dt
@@ -33,19 +34,32 @@ class MainState < Phaser::State
     @scoreText = $game.add.text(16, 16, '', { fontSize: '32px', fill: '#fff' })
     $game.stage.background_color = "F8F"
     @cookie = $game.add.sprite(
-      $game.rnd.between(0, $size_x-120),
-      $game.rnd.between(0, $size_y-120),
+      $game.rnd.between(60, $size_x-60),
+      $game.rnd.between(60, $size_y-60),
       'cookie',
     )
+    @cookie.anchor.set(0.5, 0.5)
     @coin = $game.add.audio("coin")
     @cookie.input_enabled = true
     @cookie.events.on(:down) do
       @score += 1
+      @emitter.x = @cookie.x
+      @emitter.y = @cookie.y
+      @emitter.start true, 2000, nil, 1
       @coin.play
     end
     angle = $game.math.deg_to_rad($game.rnd.between(0, 360))
     speed = 200.0
     @dx = Math.cos(angle) * speed
     @dy = Math.sin(angle) * speed
+
+    @emitter = $game.add.emitter(0, 0, 1000)
+    @emitter.make_particles('banana')
+    @emitter.gravity = -50
+    @emitter.minParticleSpeed.x = -0.1 * @dx
+    @emitter.minParticleSpeed.y = -0.1 * @dy
+    @emitter.maxParticleSpeed.x = -0.4 * @dx
+    @emitter.maxParticleSpeed.y = -0.4 * @dy
+    @emitter.set_alpha(0.5, 0.8, 0)
   end
 end
