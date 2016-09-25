9067a7bbc03a0afe7d771cd0551e6ca9db51301b
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2016-09-25 17:10:25 +0100

    Eye colors

--- app/eyes.rb
+++ app/eyes.rb
@@ -14,20 +14,28 @@ class Eye
     cx = cell_x * $size_x / 4
     cy = cell_y * $size_y / 4
     max_eye_size = [$size_x, $size_y].min / 4.0
-    @eyesize = $game.rnd.between(max_eye_size*0.2, max_eye_size*0.8)
+    @eyesize = $game.rnd.between(max_eye_size*0.3, max_eye_size*0.8)
     @x = $game.rnd.between(cx + 50, cx + $size_x / 4 - 50)
     @y = $game.rnd.between(cy + 50, cy + $size_y / 4 - 50)
     @max_eye_movement = @eyesize * 0.2
     @eye = $game.add.graphics(@x, @y)
     @eye.begin_fill(0xFFFFFF)
-    @eye.line_style(5, 0x000000, 1)
+    @eye.line_style(3, 0x000000, 1)
     @eye.draw_circle(0, 0, @eyesize)
     @eye.end_fill
     @retina = $game.add.graphics(@x, @y)
-    @retina.begin_fill(0x000000)
-    @retina.line_style(3, 0x000000, 1)
+    @retina.begin_fill(rand_retina_color)
+    # @retina.line_style(1, 0x000000, 1)
     @retina.draw_circle(0, 0, @eyesize*0.5)
     @retina.end_fill
+  end
+
+  def rand_retina_color
+    ("%02x%02x%02x" % [
+      $game.rnd.between(140, 255),
+      $game.rnd.between(90, 140),
+      $game.rnd.between(90, 140),
+    ]).to_i(16)
   end
 
   def update(mx, my)
