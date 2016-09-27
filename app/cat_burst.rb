c61026a5b26c701137c2f25eb2fbace8c7184514
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2016-09-27 18:45:16 +0100

    Refactored Cat Burst so emitter setups are separate from game state

--- app/cat_burst.rb
+++ app/cat_burst.rb
@@ -9,6 +9,43 @@ class Game
   end
 end
 
+class CatEmitter
+  def initialize
+    @emitter = $game.add.emitter(0, 0, 1000)
+    @emitter.make_particles("cat")
+    @emitter.gravity = 200
+  end
+
+  def burst_at(x, y)
+    @emitter.x = x
+    @emitter.y = y
+    # The first parameter sets the effect to "explode" which means all particles are emitted at once
+    # The second gives each particle a 2000ms lifespan
+    # The third is ignored when using burst/explode mode
+    # The final parameter (10) is how many particles will be emitted in this single burst
+    @emitter.start true, 5000, nil, 5
+  end
+end
+
+class StarEmitter
+  def initialize
+    @emitter = $game.add.emitter(0, 0, 1000)
+    @emitter.make_particles("star")
+    @emitter.gravity = -50
+    @emitter.maxParticleSpeed.x = 50
+    @emitter.minParticleSpeed.x = -50
+    @emitter.maxParticleSpeed.y = 50
+    @emitter.minParticleSpeed.y = -50
+    @emitter.set_alpha(0.2, 0.5, 0)
+  end
+
+  def burst_at(x, y)
+    @emitter.x = x
+    @emitter.y = y
+    @emitter.start true, 2000, nil, 40
+  end
+end
+
 class MainState < Phaser::State
   def preload
     $game.load.image("star", "/images/star2.png")
@@ -19,34 +56,12 @@ class MainState < Phaser::State
   def create
     $game.stage.background_color = "8FA"
     $game.physics.start_system(Phaser::Physics::ARCADE)
-
-    @emitter2 = $game.add.emitter(0, 0, 1000)
-    @emitter2.make_particles("star")
-    @emitter2.gravity = -50
-    @emitter2.maxParticleSpeed.x = 50
-    @emitter2.minParticleSpeed.x = -50
-    @emitter2.maxParticleSpeed.y = 50
-    @emitter2.minParticleSpeed.y = -50
-    @emitter2.set_alpha(0.2, 0.5, 0)
-
-    @emitter = $game.add.emitter(0, 0, 1000)
-    @emitter.make_particles("cat")
-    @emitter.gravity = 200
-
+    @star_emitter = StarEmitter.new
+    @cat_emitter = CatEmitter.new
     @coin = $game.add.audio("coin")
-
     $game.input.on("down") do |pointer, event|
-      @emitter2.x = `pointer.x`
-      @emitter2.y = `pointer.y`
-      @emitter2.start true, 2000, nil, 40
-
-      @emitter.x = `pointer.x`
-      @emitter.y = `pointer.y`
-      # The first parameter sets the effect to "explode" which means all particles are emitted at once
-      # The second gives each particle a 2000ms lifespan
-      # The third is ignored when using burst/explode mode
-      # The final parameter (10) is how many particles will be emitted in this single burst
-      @emitter.start true, 5000, nil, 5
+      @star_emitter.burst_at(`pointer.x`, `pointer.y`)
+      @cat_emitter.burst_at(`pointer.x`, `pointer.y`)
       @coin.play
       @coin.volume = 0.2
     end
