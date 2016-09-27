fff22a57e07657e4ea95d41732f96624e7d078f8
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2016-09-27 17:35:04 +0100

    Score if cat in basket or not

--- app/cat_basket.rb
+++ app/cat_basket.rb
@@ -11,11 +11,26 @@ end
 
 class Cat
   def initialize
-    @cat = $game.add.sprite($size_x/2, 0, "cat")
+    @cat = $game.add.sprite(0, 0, "cat")
+    @cat.anchor.set(0.5, 0.5)
+    send_new_cat!
+  end
+
+  def send_new_cat!
+    @cat.x = $game.rnd.between(100, $size_x-100)
+    @cat.y = 0
   end
 
   def update(dt)
-    @cat.y += dt * 100
+    @cat.y += dt * 250
+  end
+
+  def x
+    @cat.x
+  end
+
+  def y
+    @cat.y
   end
 end
 
@@ -35,6 +50,11 @@ class Basket
     if $game.input.keyboard.down?(`Phaser.KeyCode.RIGHT`)
       @basket.x += dt * 600
     end
+    @basket.x = $game.math.clamp(@basket.x, 110, $size_x-110)
+  end
+
+  def x
+    @basket.x
   end
 end
 
@@ -47,11 +67,32 @@ class MainState < Phaser::State
     $game.stage.background_color = "8FB"
     @cat = Cat.new
     @basket = Basket.new
+    @score_val = 0
+    @score = $game.add.text($size_x / 8, $size_y / 8, @score_val, { fontSize: "100px", fill: "#000", align: "center" })
+    @score.anchor.set(0.5)
+  end
+
+  def cat_in_basket!
+    @score_val += 1
+    @cat.send_new_cat!
+  end
+
+  def cat_fell_out!
+    @score_val -= 1
+    @cat.send_new_cat!
   end
 
   def update
     dt = $game.time.physics_elapsed
     @cat.update(dt)
     @basket.update(dt)
+    if @cat.y >= $size_y-10
+      if (@cat.x - @basket.x).abs <= 100
+        cat_in_basket!
+      else
+        cat_fell_out!
+      end
+    end
+    @score.text  = @score_val
   end
 end
