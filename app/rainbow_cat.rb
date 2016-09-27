fb7bb950b8e6d597dc015715274909fd7ce75262
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2016-09-27 16:20:59 +0100

    Star Cat trail

--- app/rainbow_cat.rb
+++ app/rainbow_cat.rb
@@ -14,7 +14,7 @@ class Star
     @star = $game.add.sprite(
       pos*$size_x,
       $game.rnd.between(0, $size_y),
-      'star'
+      "star"
     )
     @star.anchor.set(0.5, 0.5)
   end
@@ -40,6 +40,38 @@ class Cat
     @cat.x = ( 0.5 + 0.1 * Math.sin(@phase*2) ) * $size_x
     @cat.y = ( 0.5 + 0.05 * Math.cos(@phase*2) ) * $size_y
   end
+
+  def x
+    @cat.x
+  end
+
+  def y
+    @cat.y
+  end
+end
+
+class TrailEmitter
+  def initialize
+    @phase = 0
+    @emitter = $game.add.emitter(0, 0, 1000)
+    @emitter.make_particles("star2")
+    @emitter.gravity = 0
+    @emitter.maxParticleSpeed.x = 110
+    @emitter.minParticleSpeed.x = 90
+    @emitter.maxParticleSpeed.y = 10
+    @emitter.minParticleSpeed.y = -10
+    @emitter.set_alpha(0.2, 0.5, 0)
+  end
+
+  def update(dt, cat)
+    @emitter.x = cat.x
+    @emitter.y = cat.y
+    @phase += dt
+    if @phase > 0.05
+      @phase -= 0.05
+      @emitter.start true, 5000, nil, 1
+    end
+  end
 end
 
 class MainState < Phaser::State
@@ -47,6 +79,7 @@ class MainState < Phaser::State
     $game.load.image("cat", "/images/cat_images/cat4.png")
     $game.load.image("star", "/images/star-icon.png")
     $game.load.audio("nyan", "/audio/nyan.mp3")
+    $game.load.image("star2", "/images/star2.png")
   end
 
   def create
@@ -55,6 +88,7 @@ class MainState < Phaser::State
     @nyan_music.play
     @nyan_music.loop = true
     @stars = 50.times.map{|i| Star.new(i/50.0) }
+    @emitter = TrailEmitter.new
     @cat = Cat.new
   end
 
@@ -64,5 +98,6 @@ class MainState < Phaser::State
     @stars.each do |star|
       star.update(dt)
     end
+    @emitter.update(dt, @cat)
   end
 end
