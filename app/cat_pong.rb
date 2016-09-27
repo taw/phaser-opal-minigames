0460b2e66254c88a2ef5cdc96b579b1078e160a1
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2016-09-27 19:21:45 +0100

    Refactored blob of CatPong

--- app/cat_pong.rb
+++ app/cat_pong.rb
@@ -9,79 +9,49 @@ class Game
   end
 end
 
-class MainState < Phaser::State
-  def preload
-    $game.load.image("cat", "/images/cat_images/cat17.png")
-    $game.load.image("star", "/images/star-icon.png")
-    $game.load.audio("meow", "/audio/cat_meow.mp3")
-    $game.load.audio("meow2", "/audio/cat_meow_2.mp3")
+class Paddle
+  def initialize(position)
+    @paddle = $game.add.graphics(position, $size_y / 2)
+    @paddle.line_style(5, "white")
+    @paddle.line_style(0)
+    @paddle.begin_fill(0x000)
+    @paddle.draw_rect(0, -65, 30, 130)
   end
 
-  def hit_left_paddle?
-    (@left_paddle.y - @ball.y).abs < 65 + 25
+  def hit?(ball)
+    (@paddle.y - ball.y).abs < 65 + 25
   end
 
-  def hit_right_paddle?
-    (@right_paddle.y - @ball.y).abs < 65 + 25
+  def y
+    @paddle.y
   end
 
-  def bounce_left_paddle
-    intercept = (@left_paddle.y - @ball.y) / (65 + 25)
-    speed = 1.1 * Math.sqrt(@ball_dx*@ball_dx + @ball_dy*@ball_dy)
-    launch_ball(speed, 0 - 45*intercept)
-    bounce_effects
+  def y=(v)
+    # ensure paddle boundaries
+    @paddle.y  = $game.math.clamp(v,  75, $size_y - 75)
+  end
+end
+
+class Ball
+  def initialize(state)
+    @ball = $game.add.sprite(0, 0, "cat")
+    @ball.height = 50
+    @ball.width = 50
+    @ball.anchor.set(0.5, 0.5)
+    @state = state
+    reset!
   end
 
-  def bounce_right_paddle
-    intercept = (@right_paddle.y - @ball.y) / (65 + 25)
-    speed = 1.1 * Math.sqrt(@ball_dx*@ball_dx + @ball_dy*@ball_dy)
-    launch_ball(speed, 180 + 45*intercept)
-    bounce_effects
+  def launch!(speed, angle)
+    @dx = Math.cos($game.math.deg_to_rad(angle)) * speed
+    @dy = Math.sin($game.math.deg_to_rad(angle)) * speed
   end
 
-  def launch_ball(speed, angle)
-    @ball_dx = Math.cos($game.math.deg_to_rad(angle)) * speed
-    @ball_dy = Math.sin($game.math.deg_to_rad(angle)) * speed
+  def speed
+    (@dx**2 + @dy**2)**0.5
   end
 
-  def bounce_effects
-    @emitter.x = @ball.x
-    @emitter.y = @ball.y
-    @emitter.start true, 5000, nil, 10
-    @meow.play()
-  end
-
-  def fail_effects
-    @meow2.play
-    reset_ball()
-  end
-
-  def ensure_bounds
-    if @ball.x < 65 and @ball_dx < 0
-      if hit_left_paddle?
-        bounce_left_paddle()
-      else
-        @right_score_val += 1
-        fail_effects()
-      end
-    end
-    if @ball.x > $size_x-65 and @ball_dx > 0
-      if hit_right_paddle?
-        bounce_right_paddle
-      else
-        @left_score_val += 1
-        fail_effects()
-      end
-    end
-    if @ball.y < 25 and @ball_dy < 0
-      @ball_dy = -@ball_dy
-    end
-    if @ball.y > $size_y-25 and @ball_dy > 0
-      @ball_dy = -@ball_dy
-    end
-  end
-
-  def reset_ball
+  def reset!
     @ball.x = $size_x / 2
     @ball.y = $size_y / 2
     if $game.rnd.between(0,1) == 0
@@ -91,7 +61,61 @@ class MainState < Phaser::State
       # Left
       angle = $game.rnd.between(180-45, 180+45)
     end
-    launch_ball(300.0, angle)
+    launch!(300.0, angle)
+  end
+
+  def x
+    @ball.x
+  end
+
+  def y
+    @ball.y
+  end
+
+  def ball_reached_right_side!
+    if @right_paddle.hit?(@ball)
+      bounce_right_paddle!
+    else
+      @left_score.value += 1
+      fail_effects!
+    end
+  end
+
+  def update(dt)
+    @ball.x += dt * @dx
+    @ball.y += dt * @dy
+    if @ball.x < 65 and @dx < 0
+      @state.ball_reached_left_side!
+    elsif @ball.x > $size_x-65 and @dx > 0
+      @state.ball_reached_right_side!
+    elsif @ball.y < 25 and @dy < 0
+      @dy = -@dy
+    elsif @ball.y > $size_y-25 and @dy > 0
+      @dy = -@dy
+    end
+  end
+end
+
+class ScoreDisplay
+  attr_reader :value
+  def initialize(x,y,v=0)
+    @label = $game.add.text(x*$size_x, y*$size_y, "", { fontSize: "100px", fill: "#000", align: "center" })
+    @label.anchor.set(0.5)
+    self.value = v
+  end
+
+  def value=(v)
+    @value = v
+    @label.text = @value
+  end
+end
+
+class MainState < Phaser::State
+  def preload
+    $game.load.image("cat", "/images/cat_images/cat17.png")
+    $game.load.image("star", "/images/star-icon.png")
+    $game.load.audio("meow", "/audio/cat_meow.mp3")
+    $game.load.audio("meow2", "/audio/cat_meow_2.mp3")
   end
 
   def create
@@ -105,43 +129,64 @@ class MainState < Phaser::State
       @grid.line_to(0, y + 10)
     end
 
-    # score display
-    @left_score_val  = 0
-    @right_score_val = 0
+    @left_score = ScoreDisplay.new(0.25, 0.125)
+    @right_score = ScoreDisplay.new(0.75, 0.125)
 
-    @left_score = $game.add.text($size_x / 4, $size_y / 8, @left_score_val, { fontSize: "100px", fill: "#000", align: "center" })
-    @left_score.anchor.set(0.5)
-
-    @right_score = $game.add.text($size_x / 4 * 3, $size_y / 8, @right_score_val, { fontSize: "100px", fill: "#000", align: "center" })
-    @right_score.anchor.set(0.5)
-
-    # paddles
-    @left_paddle = $game.add.graphics(10, $size_y / 2)
-    @left_paddle.line_style(5, "white")
-    @left_paddle.line_style(0)
-    @left_paddle.begin_fill(0x000)
-    @left_paddle.draw_rect(0, -65, 30, 130)
-
-    @right_paddle = $game.add.graphics($size_x - 40, $size_y / 2)
-    @right_paddle.line_style(5, "white")
-    @right_paddle.line_style(0)
-    @right_paddle.begin_fill(0x000)
-    @right_paddle.draw_rect(0, -65, 30, 130)
+    @left_paddle = Paddle.new(10)
+    @right_paddle = Paddle.new($size_x - 40)
 
     # star emitter
     @emitter = $game.add.emitter(0, 0, 1000)
     @emitter.make_particles("star")
     @emitter.gravity = 200
 
-    # ball
-    @ball = $game.add.sprite(0, 0, "cat")
-    @ball.height = 50
-    @ball.width = 50
-    @ball.anchor.set(0.5, 0.5)
-    reset_ball
-
+    @ball = Ball.new(self)
     @meow = $game.add.audio("meow")
     @meow2 = $game.add.audio("meow2")
+  end
+
+  def bounce_effects!
+    @emitter.x = @ball.x
+    @emitter.y = @ball.y
+    @emitter.start true, 5000, nil, 10
+    @meow.play
+  end
+
+  def fail_effects!
+    @meow2.play
+    @ball.reset!
+  end
+
+  def bounce_left_paddle!
+    intercept = (@left_paddle.y - @ball.y) / (65 + 25)
+    speed = 1.1 * @ball.speed
+    @ball.launch!(speed, 0 - 45*intercept)
+    bounce_effects!
+  end
+
+  def bounce_right_paddle!
+    intercept = (@right_paddle.y - @ball.y) / (65 + 25)
+    speed = 1.1 * @ball.speed
+    @ball.launch!(speed, 180 + 45*intercept)
+    bounce_effects!
+  end
+
+  def ball_reached_left_side!
+    if @left_paddle.hit?(@ball)
+      bounce_left_paddle!
+    else
+      @right_score.value += 1
+      fail_effects!
+    end
+  end
+
+  def ball_reached_right_side!
+    if @right_paddle.hit?(@ball)
+      bounce_right_paddle!
+    else
+      @left_score.value += 1
+      fail_effects!
+    end
   end
 
   def update
@@ -149,31 +194,16 @@ class MainState < Phaser::State
     dt = $game.time.physics_elapsed
     if $game.input.keyboard.down?(`Phaser.KeyCode.W`)
       @left_paddle.y -= dt * 600
-    end
-    if $game.input.keyboard.down?(`Phaser.KeyCode.S`)
+    elsif $game.input.keyboard.down?(`Phaser.KeyCode.S`)
       @left_paddle.y += dt * 600
     end
 
     if $game.input.keyboard.down?(`Phaser.KeyCode.UP`)
       @right_paddle.y -= dt * 600
-    end
-    if $game.input.keyboard.down?(`Phaser.KeyCode.DOWN`)
+    elsif $game.input.keyboard.down?(`Phaser.KeyCode.DOWN`)
       @right_paddle.y += dt * 600
     end
 
-    # ensure paddle boundaries
-    @left_paddle.y  = $game.math.clamp(@left_paddle.y,  75, $size_y - 75)
-    @right_paddle.y = $game.math.clamp(@right_paddle.y, 75, $size_y - 75)
-
-    # move ball
-    @ball.x += dt * @ball_dx
-    @ball.y += dt * @ball_dy
-
-    # ball boundary check
-    ensure_bounds
-
-    # udpate scores
-    @left_score.text  = @left_score_val
-    @right_score.text = @right_score_val
+    @ball.update(dt)
   end
 end
