99762cfdd1560f47fb797ff12aeb3e483dcfe72a
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2016-09-28 23:50:16 +0100

    Cat Tweens (fixed from CS)

---
+++ app/cat_tweens.rb
@@ -0,0 +1,47 @@
+require_relative "common"
+
+class Game
+  def initialize
+    $game = Phaser::Game.new(width: $size_x, height: $size_y)
+    $game.state.add(:main, MainState.new, true)
+  end
+end
+
+class MainState < Phaser::State
+  def preload
+    (1..20).each do |i|
+      $game.load.image("cat#{i}", "/images/cat_images/cat#{i}.png")
+    end
+  end
+
+  def rand_x
+    $game.rnd.between(100, $size_x - 100)
+  end
+
+  def rand_y
+    $game.rnd.between(100, $size_y - 100)
+  end
+
+  def create
+    $game.stage.background_color = "F88"
+    (1..5).each do |i|
+      new_cat_and_tween(i)
+    end
+  end
+
+  def new_tween(cat)
+    tween = $game.add.tween(cat)
+    tween.on(:complete) do
+      new_tween(cat)
+    end
+    x = rand_x
+    y = rand_y
+    tween.to(properties: {x: x, y: y}, duration: 1000, auto_start: true)
+  end
+
+  def new_cat_and_tween(i)
+    cat = $game.add.sprite(rand_x, rand_y, "cat#{i}")
+    cat.anchor.set(0.5, 0.5)
+    new_tween(cat)
+  end
+end
