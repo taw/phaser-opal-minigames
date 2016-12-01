b7edd8d79da990180cb00ed1b8d7190eef68bce3
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2016-12-01 21:53:21 +0000

    Give forest walk some background

--- app/forest_walk.rb
+++ app/forest_walk.rb
@@ -2,13 +2,29 @@ require_relative "common"
 
 class MainState < Phaser::State
   def preload
-    # All seem to be off by 1 pixel :-/
     $game.load.spritesheet("angel", "../images/characters/angel-blue-2.png", 24+2, 32+2, 12)
+    # 128 x 30
+    $game.load.spritesheet("angband", "../images/angband.png", 32+2, 32+2)
   end
 
-  def create
-    $game.stage.background_color = "4A6"
-    $game.physics.start_system(Phaser::Physics::ARCADE)
+  def create_map
+    40.times do |x|
+      40.times do |y|
+        if rand > 0.5
+          # Plains
+          frame = 23*128 + [9,10,11].sample
+        else
+          # Trees
+          frame = 23*128 + 54 + (0..8).to_a.sample
+        end
+        tile = $game.add.sprite(x*64, y*64, "angband")
+        tile.scale.set(2)
+        tile.frame = frame
+      end
+    end
+  end
+
+  def create_player
     @player = $game.add.sprite($size_x/2, $size_y/2, "angel")
     @player.anchor.set(0.5)
     @player.animations.add("up", [0,1,2], 1800, true)
@@ -18,6 +34,12 @@ class MainState < Phaser::State
     @player.scale.set(2)
     $game.physics.enable(@player, Phaser::Physics::ARCADE)
     @player.body.collide_world_bounds = true
+  end
+
+  def create
+    $game.physics.start_system(Phaser::Physics::ARCADE)
+    create_map
+    create_player
     @cursors = $game.input.keyboard.create_cursor_keys
   end
 
