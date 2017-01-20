f36ed008190c04ec23308f03569c0bf1eb183b53
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2017-01-20 19:41:07 +0000

    A bit of randomness and state machine

--- app/zombie_wave.rb
+++ app/zombie_wave.rb
@@ -1,52 +1,71 @@
 require_relative "common"
 
-class Zombie
+class Monster
   attr_reader :sprite
+
+  def set_state!(state)
+    @state = state
+    if @state == "pre-appear"
+      @delay = rand*2
+      @sprite.visible = false
+    else
+      @sprite.visible = true
+      @sprite.animations.play(state)
+    end
+  end
+
+  def update(dt)
+    current = @sprite.animations.current_anim
+    case @state
+    when "pre-appear"
+      @delay -= dt
+      set_state! "appear" if @delay < 0
+    when "appear"
+      return unless current.finished?
+      set_state! "walk"
+    when "walk"
+      @sprite.x += dt * walking_speed_x
+      set_state! "walk" if current.finished?
+    else
+      raie "Unknown state #{@state}"
+    end
+  end
+end
+
+class Zombie < Monster
   def initialize(x,y)
     @sprite = $game.add.sprite(x, y, "zombie")
     @sprite.anchor.set(0.5)
     @sprite.scale.set(0.25)
     @sprite.animations.add("appear", (1..11).to_a, 10, false)
-    @sprite.animations.add("attack", (12..18).to_a, 10, true)
+    @sprite.animations.add("attack", (12..18).to_a, 10, false)
     @sprite.animations.add("die", (19..26).to_a, 10, false)
-    @sprite.animations.add("idle", (27..32).to_a, 10, true)
-    @sprite.animations.add("walk", (33..42).to_a, 10, true)
+    @sprite.animations.add("idle", (27..32).to_a, 10, false)
+    @sprite.animations.add("walk", (33..42).to_a, 10, false)
     @sprite.animations.play("appear")
+    set_state! "pre-appear"
   end
 
-  def update(dt)
-    current = @sprite.animations.current_anim
-    if current.name == "appear"
-      return unless current.finished?
-      @sprite.animations.play("walk")
-    elsif current.name == "walk"
-      @sprite.x -= dt * 40
-    end
+  def walking_speed_x
+    -40
   end
 end
 
-class Skeleton
-  attr_reader :sprite
+class Skeleton < Monster
   def initialize(x, y)
     @sprite = $game.add.sprite(x, y, "skeleton")
     @sprite.anchor.set(0.5)
     @sprite.scale.set(-0.25, 0.25)
     @sprite.animations.add("appear", (1..10).to_a, 10, false)
-    @sprite.animations.add("attack", (11..18).to_a, 10, true)
+    @sprite.animations.add("attack", (11..18).to_a, 10, false)
     @sprite.animations.add("die", (19..26).to_a, 10, false)
-    @sprite.animations.add("idle", (27..32).to_a, 10, true)
-    @sprite.animations.add("walk", (33..42).to_a, 10, true)
-    @sprite.animations.play("appear")
+    @sprite.animations.add("idle", (27..32).to_a, 10, false)
+    @sprite.animations.add("walk", (33..42).to_a, 10, false)
+    set_state! "pre-appear"
   end
 
-  def update(dt)
-    current = @sprite.animations.current_anim
-    if current.name == "appear"
-      return unless current.finished?
-      @sprite.animations.play("walk")
-    elsif current.name == "walk"
-      @sprite.x += dt * 40
-    end
+  def walking_speed_x
+    40
   end
 end
 
@@ -61,10 +80,10 @@ class MainState < Phaser::State
     @zombies = []
     @skeletons = []
     (2..6).each do |i|
-      @skeletons << Skeleton.new($size_x * 0.25, $size_y * i / 8)
+      @skeletons << Skeleton.new($size_x * (0.05 + rand*0.2), $size_y * i / 8)
     end
     (2..6).each do |i|
-      @zombies << Zombie.new($size_x * 0.75, $size_y * i / 8)
+      @zombies << Zombie.new($size_x * (0.95 - rand*0.2), $size_y * i / 8)
     end
   end
 
